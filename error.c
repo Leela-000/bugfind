217883d8d74cce676c397ebc25726f64018643bb
Leela Borra <leelaborra0@gmail.com>
2025-09-21 21:04:16 +0530

    Update error.c

--- error.c
+++ error.c
@@ -10,7 +10,7 @@ void merge(int arr[], int l, int m, int r) {
     for(int i=0;i<n1;i++)
         L[i] = arr[l + i];
     for(int j=0;j<n2;j++)
-        R[j] = arr[j]; 
+        R[j] = arr[(m+1)+j]; 
 
     int i=0,j=0,k=l;
     while(i<n1 && j<n2) {
@@ -20,8 +20,8 @@ void merge(int arr[], int l, int m, int r) {
             arr[k++] = R[j++];
     }
 
-    while(i<n1) arr[k++] = L[i++];
-    while(j<n2) arr[k++] = R[j++];
+    while(i<n1){arr[k++]=L[i++];}
+    while(j<n2){arr[k++]=R[j++];}
 }
 
 void mergeSort(int arr[], int l, int r) {
